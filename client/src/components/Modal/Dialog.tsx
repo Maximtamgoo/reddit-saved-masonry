2d4aac4d345797fcd00255a24e6bb828af9b38a3
Maximtamgoo <maximtamgoo@gmail.com>
2025-03-04 13:28:39 -0500

    replace useeffect with ref callback

--- client/src/components/Modal/Dialog.tsx
+++ client/src/components/Modal/Dialog.tsx
@@ -1,6 +1,6 @@
 import ArrowLeft from "@src/svg/arrow-left.svg?react";
 import { cn } from "@src/utils/cn";
-import { type PropsWithChildren, useEffect, useRef } from "react";
+import { type PropsWithChildren } from "react";
 import { createPortal } from "react-dom";
 import style from "./Modal.module.css";
 
@@ -8,27 +8,23 @@ type Props = {
   onClose: () => void;
 };
 
+function openModal(node: HTMLDialogElement) {
+  const paddingRight = document.body.style.paddingRight;
+  const overflowY = document.body.style.overflowY;
+  const scrollbarWidth = window.innerWidth - document.body.clientWidth + "px";
+  document.body.style.paddingRight = scrollbarWidth;
+  document.body.style.overflowY = "hidden";
+  node.showModal();
+  node.focus();
+  return () => {
+    document.body.style.paddingRight = paddingRight;
+    document.body.style.overflowY = overflowY;
+  };
+}
+
 export default function Dialog({ onClose, children }: PropsWithChildren<Props>) {
-  const ref = useRef<HTMLDialogElement>(null);
-
-  useEffect(() => {
-    if (ref.current) {
-      const paddingRight = document.body.style.paddingRight;
-      const overflowY = document.body.style.overflowY;
-      const scrollbarWidth = window.innerWidth - document.body.clientWidth + "px";
-      document.body.style.paddingRight = scrollbarWidth;
-      document.body.style.overflowY = "hidden";
-      ref.current.showModal();
-      ref.current.focus();
-      return () => {
-        document.body.style.paddingRight = paddingRight;
-        document.body.style.overflowY = overflowY;
-      };
-    }
-  }, []);
-
   return createPortal(
-    <dialog ref={ref} onClick={onClose} onClose={onClose} className={style.dialog}>
+    <dialog ref={openModal} onClick={onClose} onClose={onClose} className={style.dialog}>
       {children}
       <button className={cn(style.modal_btn, style.close)} onClick={onClose}>
         <ArrowLeft />
--- client/src/components/Modal/Playable.tsx
+++ client/src/components/Modal/Playable.tsx
@@ -1,25 +1,19 @@
-import { useEffect, useRef } from "react";
-
 type Props = {
   src: string;
   poster: string;
   onError: () => void;
 };
 
+function pauseOnHidden(node: HTMLVideoElement) {
+  const onChange = () => document.hidden && node.pause();
+  document.addEventListener("visibilitychange", onChange);
+  return () => document.removeEventListener("visibilitychange", onChange);
+}
+
 export default function Playable({ src, poster, onError }: Props) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    function onChange() {
-      if (document.hidden && videoRef.current) videoRef.current.pause();
-    }
-    document.addEventListener("visibilitychange", onChange);
-    return () => document.removeEventListener("visibilitychange", onChange);
-  }, []);
-
   return (
     <video
-      ref={videoRef}
+      ref={pauseOnHidden}
       src={src}
       poster={poster}
       autoPlay={true}
