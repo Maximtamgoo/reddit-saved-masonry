44317f2515eb93f81aac162e407e7e3179dc71ce
Maximtamgoo <maximtamgoo@gmail.com>
2025-08-20 13:16:30 -0400

    handle boolean & undefined classnames

--- client/src/utils/cn.ts
+++ client/src/utils/cn.ts
@@ -1,1 +1,11 @@
-export const cn = (...classes: string[]) => classes.join(" ");
+export function cn(...classes: (string | false | undefined)[]) {
+  let classStr = "";
+  for (const c of classes) {
+    if (c === undefined) continue;
+    if (typeof c === "boolean") continue;
+    if (typeof c === "string") {
+      classStr += " " + c;
+    }
+  }
+  return classStr === "" ? undefined : classStr.trimStart();
+}
