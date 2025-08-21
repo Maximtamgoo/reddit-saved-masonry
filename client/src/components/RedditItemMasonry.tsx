8cbaa592fd25522d552ff810ca71aab280779816
Maximtamgoo <maximtamgoo@gmail.com>
2025-08-21 16:58:23 -0400

    Masonry component refactor with masonry-max-width calc

---
+++ client/src/components/RedditItemMasonry.tsx
@@ -0,0 +1,125 @@
+import { useCallback, useEffect, useMemo, type ReactNode } from "react";
+import type { RedditItem } from "@src/schema/RedditItem";
+import Card from "./Card/Card";
+import { calculateAspectRatioFit } from "@src/utils/calculateAspectRatioFit";
+import { useResizeObserver } from "@src/hooks/useResizeObserver";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
+
+type Props = {
+  items: RedditItem[];
+  maxLanes?: number;
+  minLaneWidth: number;
+  maxLaneWidth: number;
+  minCardHeight: number;
+  maxCardHeight: number;
+  loadMore: () => void;
+  renderLoader: ReactNode;
+};
+
+export function RedditItemMasonry({
+  items,
+  maxLanes,
+  minLaneWidth,
+  maxLaneWidth,
+  minCardHeight,
+  maxCardHeight,
+  loadMore,
+  renderLoader,
+}: Props) {
+  const { ref, rect } = useResizeObserver();
+  const parentWidth = rect.width;
+
+  // const gap = useMemo(() => (parentWidth < 1175 ? 16 : 24), [parentWidth]);
+  const gap = 20;
+
+  const lanes = useMemo(() => {
+    const lanes = Math.floor((parentWidth + gap) / (minLaneWidth + gap));
+    return Math.max(1, Math.min(lanes, maxLanes ?? Infinity));
+  }, [parentWidth, gap, minLaneWidth, maxLanes]);
+
+  const itemWidth = useMemo(() => {
+    const itemWidth = Math.floor((parentWidth - gap * (lanes - 1)) / lanes);
+    return Math.min(itemWidth, maxLaneWidth);
+  }, [parentWidth, gap, lanes, maxLaneWidth]);
+
+  const maxWidth = useMemo(() => {
+    const gapTotal = gap * (lanes - 1);
+    const maxWidth = itemWidth * lanes + gapTotal;
+    document.documentElement.style.setProperty("--masonry-max-width", maxWidth + "px");
+    return maxWidth;
+  }, [gap, itemWidth, lanes]);
+
+  const winVirtualizer = useWindowVirtualizer({
+    enabled: parentWidth > 0,
+    count: items.length,
+    lanes,
+    gap,
+    overscan: 20,
+    getItemKey: useCallback(
+      (i: number) => {
+        // if (i === 0) console.log("getItemKey 0");
+        return items[i].id;
+      },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [items, lanes, itemWidth, maxCardHeight],
+    ),
+    estimateSize: useCallback(
+      (i: number) => {
+        // if (i === 0) console.log("estimateSize 0");
+        const item = items[i];
+        const minHeight = minCardHeight;
+        const maxHeight = maxCardHeight;
+        const detailsHeight = 100;
+        let totalHeight = detailsHeight;
+        if (item.type === "playable" || item.type === "image" || item.type === "gallery") {
+          const p = item.preview;
+          totalHeight += calculateAspectRatioFit(p.width, p.height, itemWidth, p.height).height;
+        }
+        return Math.max(minHeight, Math.min(maxHeight, Math.floor(totalHeight)));
+      },
+      [items, itemWidth, minCardHeight, maxCardHeight],
+    ),
+  });
+
+  const virtualItems = winVirtualizer.getVirtualItems();
+
+  useEffect(() => {
+    const lastItem = virtualItems[virtualItems.length - 1];
+    if (lastItem && lastItem.index === items.length - 1) loadMore();
+  }, [virtualItems, items.length, loadMore]);
+
+  return (
+    <main ref={ref} style={{ maxWidth: "100%", padding: "var(--space-2)" }}>
+      <div
+        style={{
+          position: "relative",
+          margin: "auto",
+          maxWidth: maxWidth + "px",
+          width: "100%",
+          height: winVirtualizer.getTotalSize() + "px",
+        }}
+      >
+        {virtualItems.map((item) => {
+          return (
+            <div
+              key={item.key}
+              data-index={item.index}
+              style={{
+                position: "absolute",
+                top: 0,
+                translate: `0 ${item.start - winVirtualizer.options.scrollMargin}px`,
+                left: item.lane * itemWidth + "px",
+                width: itemWidth + "px",
+                marginLeft: item.lane * gap + "px",
+                height: item.size + "px",
+              }}
+            >
+              <Card item={items[item.index]} />
+            </div>
+          );
+        })}
+      </div>
+      {winVirtualizer.options.enabled && renderLoader}
+    </main>
+  );
+}
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,37 +1,19 @@
-import Card from "@src/components/Card/Card";
-import Loader from "@src/components/Loader";
-import VirtualMasonry from "@src/components/VirtualMasonry";
-import { useResizeObserver } from "@src/hooks/useResizeObserver";
-import { RedditItem } from "@src/schema/RedditItem";
-import { useGetSavedContent } from "@src/services/queries";
-import { calculateAspectRatioFit } from "@src/utils/calculateAspectRatioFit";
 import { useCallback, useMemo, useRef } from "react";
+import { useGetSavedContent } from "@src/services/queries";
+import { RedditItemMasonry } from "@src/components/RedditItemMasonry";
+import Loader from "@src/components/Loader";
+import { useSettingsStore } from "@src/store/settings";
+const { minCardSize } = useSettingsStore.getInitialState();
 
 export default function MainPage() {
   const isBusyRef = useRef(false);
-  const { ref, rect } = useResizeObserver();
+  const maxLanes = useSettingsStore((s) => s.maxLanes);
+  const maxCardWidth = useSettingsStore((s) => s.maxCardWidth);
+  const maxCardHeight = useSettingsStore((s) => s.maxCardHeight);
   const query = useGetSavedContent();
   const { data, isLoading, isLoadingError, isError, hasNextPage, fetchNextPage } = query;
 
   const redditItems = useMemo(() => data?.pages.flatMap((page) => page.redditItems) ?? [], [data]);
-
-  const gap = useMemo(() => (rect.width < 1175 ? 16 : 24), [rect.width]);
-
-  const getItemKey = useCallback((item: RedditItem) => item.id, []);
-
-  const estimateSize = useCallback((item: RedditItem, width: number) => {
-    const minHeight = 300;
-    const maxHeight = (window.screen.availHeight / 100) * 75;
-    const detailsHeight = 100;
-    let totalHeight = detailsHeight;
-
-    if (item.type === "playable" || item.type === "image" || item.type === "gallery") {
-      const p = item.preview;
-      totalHeight += calculateAspectRatioFit(p.width, p.height, width, p.height).height;
-    }
-
-    return Math.max(minHeight, Math.min(maxHeight, Math.floor(totalHeight)));
-  }, []);
 
   const loadMore = useCallback(async () => {
     if (!isBusyRef.current && hasNextPage && !isError) {
@@ -43,33 +25,29 @@ export default function MainPage() {
 
   if (isLoadingError || isLoading) {
     return (
-      <main style={{ height: "45vh", alignContent: "end" }}>
+      <div style={{ height: "calc(100vh - 53px)", alignContent: "center" }}>
         <Loader isError={isLoadingError} onClick={fetchNextPage} />
-      </main>
+      </div>
     );
   }
 
   return (
-    <main ref={ref}>
-      <VirtualMasonry
-        items={redditItems}
-        minLaneWidth={300}
-        maxLanes={3}
-        gap={gap}
-        overscan={20}
-        getItemKey={getItemKey}
-        estimateSize={estimateSize}
-        loadMore={loadMore}
-        renderItem={(item) => <Card item={item} />}
-        renderLoader={
-          <Loader
-            isError={isError}
-            isEnd={!hasNextPage}
-            endMessage="Reached the Reddit limit..."
-            onClick={fetchNextPage}
-          />
-        }
-      />
-    </main>
+    <RedditItemMasonry
+      items={redditItems}
+      maxLanes={maxLanes}
+      minLaneWidth={minCardSize}
+      maxLaneWidth={maxCardWidth}
+      minCardHeight={minCardSize}
+      maxCardHeight={maxCardHeight}
+      loadMore={loadMore}
+      renderLoader={
+        <Loader
+          isError={isError}
+          isEnd={!hasNextPage}
+          endMessage="Reached the Reddit limit..."
+          onClick={fetchNextPage}
+        />
+      }
+    />
   );
 }
