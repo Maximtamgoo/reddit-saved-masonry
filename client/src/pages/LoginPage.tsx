0e9a53b6a340583e47f9bc5dcc4d72a81410c305
Maximtamgoo <maximtamgoo@gmail.com>
2025-10-02 17:02:03 -0400

    use css modules in LoginPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,46 +1,14 @@
-import { css } from "@acab/ecsstatic";
+import styles from "./LoginPage.module.css";
 import Bookmark from "@src/svg/bookmark.svg?react";
-
-const login = css`
-  text-align: center;
-  padding: var(--space-2);
-  align-content: end;
-  height: 50vh;
-`;
-
-const title = css`
-  color: var(--primary);
-  font-size: var(--text-4xl);
-  text-wrap: balance;
-  & > svg {
-    display: inline;
-    vertical-align: middle;
-    margin-top: -6px;
-    width: var(--space-9);
-    height: var(--space-9);
-  }
-`;
-
-const btn = css`
-  display: block;
-  margin: var(--space-10) auto;
-  border-radius: var(--rounded-lg);
-  background-color: var(--btn-bg);
-  width: var(--space-40);
-  height: var(--space-10);
-  &:hover {
-    background-color: var(--btn-hover);
-  }
-`;
 
 export default function LoginPage() {
   return (
-    <main className={login}>
-      <div className={title}>
+    <main className={styles.login}>
+      <div className={styles.title}>
         <Bookmark />
         Reddit Saved Masonry
       </div>
-      <button className={btn} onClick={() => (window.location.href = "/api/authurl")}>
+      <button className={styles.btn} onClick={() => (window.location.href = "/api/authurl")}>
         Sign in with Reddit
       </button>
       View your saved Reddit posts in a masonry grid.
