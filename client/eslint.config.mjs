442af26fc94849167d3760267f5db57da74838fb
Maximtamgoo <maximtamgoo@gmail.com>
2025-10-20 15:17:39 -0400

    eslint react hooks flat config

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     rootConfig,
     react.configs.flat.recommended,
     react.configs.flat["jsx-runtime"],
-    reactHooks.configs["recommended-latest"],
+    reactHooks.configs.flat.recommended,
     reactRefresh.configs.recommended,
     pluginQuery.configs["flat/recommended"],
   ],
