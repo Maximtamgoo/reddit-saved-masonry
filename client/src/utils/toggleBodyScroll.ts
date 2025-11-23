a00da701bb492990cbb3d021deb36edc0d4853d0
Maximtamgoo <maximtamgoo@gmail.com>
2025-11-23 13:04:11 -0500

    add toggle method

--- client/src/utils/toggleBodyScroll.ts
+++ client/src/utils/toggleBodyScroll.ts
@@ -3,14 +3,31 @@ const paddingRight = document.body.style.paddingRight;
 const scrollbarWidth = window.innerWidth - document.body.clientWidth + "px";
 
 export const bodyScroll = (() => {
+  let locked = false;
+
+  function lock() {
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingRight = scrollbarWidth;
+    locked = true;
+  }
+
+  function unlock() {
+    document.body.style.overflow = overflow;
+    document.body.style.paddingRight = paddingRight;
+    locked = false;
+  }
+
+  function toggle() {
+    if (locked) {
+      unlock();
+    } else {
+      lock();
+    }
+  }
+
   return {
-    lock() {
-      document.body.style.overflow = "hidden";
-      document.body.style.paddingRight = scrollbarWidth;
-    },
-    unlock() {
-      document.body.style.overflow = overflow;
-      document.body.style.paddingRight = paddingRight;
-    },
+    lock,
+    unlock,
+    toggle,
   };
 })();
