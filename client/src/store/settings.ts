8da6ebd04df78fe157dfcfa4586362470abbd895
Maximtamgoo <maximtamgoo@gmail.com>
2026-01-08 14:38:52 -0500

    temp disabled zustand persist

--- client/src/store/settings.ts
+++ client/src/store/settings.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+// import { persist } from "zustand/middleware";
 
 interface State {
   // static values
@@ -22,44 +22,31 @@ const minCardSize = 350;
 const initCardWidth = Math.floor((window.screen.availWidth / 100) * 25);
 const initCardHeight = Math.floor((window.screen.availHeight / 100) * 75);
 
-export const useSettingsStore = create<State>()(
-  persist(
-    (set, _, store) => ({
-      // static values
-      maxLanesLimit: 5,
-      minCardSize,
-      // user values
-      maxLanes: 3,
-      maxCardWidth: Math.max(minCardSize, initCardWidth),
-      maxCardHeight: Math.max(minCardSize, initCardHeight),
-      actions: {
-        setMaxLanes(maxLanes) {
-          set({ maxLanes });
-        },
-        setMaxCardWidth(px) {
-          set({ maxCardWidth: px });
-        },
-        setMaxCardHeight(px) {
-          set({ maxCardHeight: px });
-        },
-        resetCardSizes() {
-          //! getInitialState might be from localstorage and not reset to actual default
-          const { maxCardWidth, maxCardHeight } = store.getInitialState();
-          set({ maxCardWidth, maxCardHeight });
-        },
-        resetMaxLanes() {
-          set({ maxLanes: store.getInitialState().maxLanes });
-        },
-      },
-    }),
-    {
-      name: "settings",
-      partialize: (s) => ({
-        // maxLanes: s.maxLanes,
-        //! resetCardSizes issue
-        // maxCardWidth: s.maxCardWidth,
-        // maxCardHeight: s.maxCardHeight,
-      }),
+export const useSettingsStore = create<State>()((set, _, store) => ({
+  // static values
+  maxLanesLimit: 5,
+  minCardSize,
+  // user values
+  maxLanes: 3,
+  maxCardWidth: Math.max(minCardSize, initCardWidth),
+  maxCardHeight: Math.max(minCardSize, initCardHeight),
+  actions: {
+    setMaxLanes(maxLanes) {
+      set({ maxLanes });
     },
-  ),
-);
+    setMaxCardWidth(px) {
+      set({ maxCardWidth: px });
+    },
+    setMaxCardHeight(px) {
+      set({ maxCardHeight: px });
+    },
+    resetCardSizes() {
+      //! getInitialState might be from localstorage and not reset to actual default
+      const { maxCardWidth, maxCardHeight } = store.getInitialState();
+      set({ maxCardWidth, maxCardHeight });
+    },
+    resetMaxLanes() {
+      set({ maxLanes: store.getInitialState().maxLanes });
+    },
+  },
+}));
