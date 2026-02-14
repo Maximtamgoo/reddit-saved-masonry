87b93785c42ea104154dd171e0f0c092ee3fde40
Maximtamgoo <maximtamgoo@gmail.com>
2026-02-14 15:42:34 -0500

    renaming theme functions

--- client/src/components/Settings/Theme.tsx
+++ client/src/components/Settings/Theme.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styles from "./Theme.module.css";
-import settingsStyles from "./SettingsPopover.module.css";
+import settingsStyles from "./Settings.module.css";
 import Sun from "@src/svg/sun.svg?react";
 import Moon from "@src/svg/moon.svg?react";
 import Monitor from "@src/svg/monitor.svg?react";
@@ -8,28 +8,28 @@ import Monitor from "@src/svg/monitor.svg?react";
 const themes = ["light", "auto", "dark"] as const;
 type Theme = (typeof themes)[number];
 
-function getInitialTheme() {
+function getTheme() {
   const localTheme = localStorage.getItem("theme") ?? "";
   const isTheme = themes.includes(localTheme as Theme);
   return isTheme ? (localTheme as Theme) : "auto";
 }
 
-function updateTheme(theme: Theme) {
+function setTheme(theme: Theme) {
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
 }
 
-updateTheme(getInitialTheme());
+setTheme(getTheme());
 
 export function Theme() {
-  const [selectedTheme, setSelectedTheme] = useState(getInitialTheme);
+  const [selectedTheme, setSelectedTheme] = useState(getTheme);
 
   function onClick(theme: Theme) {
     setSelectedTheme(theme);
     if (document.startViewTransition) {
-      document.startViewTransition(() => updateTheme(theme));
+      document.startViewTransition(() => setTheme(theme));
     } else {
-      updateTheme(theme);
+      setTheme(theme);
     }
   }
 
