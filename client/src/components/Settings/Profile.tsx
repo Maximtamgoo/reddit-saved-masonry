c0853f9870fcfb8a2d7ac939202e4d3f83c74002
Maximtamgoo <maximtamgoo@gmail.com>
2026-03-09 13:49:52 -0400

    change Profile data hook to useUser

--- client/src/components/Settings/Profile.tsx
+++ client/src/components/Settings/Profile.tsx
@@ -1,19 +1,19 @@
 import styles from "./Profile.module.css";
-import { useUserData, useSignOut } from "@src/services/queries";
+import { useUser, useSignOut } from "@src/services/queries";
 import Link from "@src/components/Link";
 import { cn } from "@src/utils/cn";
 
 export function Profile() {
-  const data = useUserData();
+  const user = useUser().data;
   const { mutate } = useSignOut();
 
-  if (!data) return null;
+  if (!user) return null;
 
   return (
     <div className={styles.profile}>
-      <Link className={styles.link} href={`https://www.reddit.com/user/${data.name}/saved`}>
-        {data.icon_img && <img className="btn" src={data.icon_img} />}
-        {data.name}
+      <Link className={styles.link} href={`https://www.reddit.com/user/${user.name}/saved`}>
+        {user.icon_img && <img className="btn" src={user.icon_img} />}
+        {user.name}
       </Link>
       <button className={cn("btn", styles.signOut)} onClick={() => mutate()}>
         Sign Out
