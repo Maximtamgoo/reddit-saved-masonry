64fd39fc00fa19d5d616ee73ddaf4eccff29631a
Maximtamgoo <maximtamgoo@gmail.com>
2026-03-13 17:54:40 -0400

    Card loading indicator

--- client/src/components/Card/Preview.tsx
+++ client/src/components/Card/Preview.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "./Preview.module.css";
+import FileImage from "@src/svg/file-image.svg?react";
 import Play from "@src/svg/play.svg?react";
 import Unknown from "./Unknown";
 
@@ -11,16 +12,26 @@ type Props = {
 
 export default function Preview({ url, isPlayable, galleryLength }: Props) {
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   if (isError) return <Unknown />;
+
   return (
     <div className={styles.preview}>
-      {galleryLength > 1 && <div className={styles.gallery}>{galleryLength}</div>}
-      {isPlayable && (
-        <div className={styles.playBtn}>
+      {galleryLength > 1 && <div className={styles.gallery_length}>{galleryLength}</div>}
+      {!isLoading && isPlayable && (
+        <div className={styles.play}>
           <Play />
         </div>
       )}
-      <img src={url} onError={() => setIsError(true)} alt="Reddit Content" />
+      {isLoading && <FileImage className={styles.loader} />}
+      <img
+        style={{ visibility: isLoading ? "hidden" : "visible" }}
+        src={url}
+        onLoad={() => setIsLoading(false)}
+        onError={() => setIsError(true)}
+        alt="Reddit Content"
+      />
     </div>
   );
 }
