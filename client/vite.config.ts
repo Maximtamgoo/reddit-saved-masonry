e3940d64acd8fa40028931d63489fb96562184de
Maximtamgoo <maximtamgoo@gmail.com>
2026-03-13 11:30:17 -0400

    vite 8 changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import checker from "vite-plugin-checker";
 import Sonda from "sonda/vite";
