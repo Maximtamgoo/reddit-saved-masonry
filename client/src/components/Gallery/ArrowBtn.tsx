8d672a8fd724b9bca3a74ab963e1077725aacc62
Maximtamgoo <maximtamgoo@gmail.com>
2026-03-22 17:26:09 -0400

    redundant props

--- client/src/components/Gallery/ArrowBtn.tsx
+++ client/src/components/Gallery/ArrowBtn.tsx
@@ -2,19 +2,12 @@ import ChevronLeft from "@src/svg/chevron-left.svg?react";
 import type { ComponentProps } from "react";
 
 type Props = {
-  disabled?: boolean;
   direction: "left" | "right";
-  onClick: () => void;
 };
 
-export default function ArrowBtn({
-  className,
-  disabled = false,
-  direction,
-  onClick,
-}: ComponentProps<"button"> & Props) {
+export default function ArrowBtn({ direction, ...props }: ComponentProps<"button"> & Props) {
   return (
-    <button className={className} disabled={disabled} data-direction={direction} onClick={onClick}>
+    <button data-direction={direction} {...props}>
       <ChevronLeft />
     </button>
   );
