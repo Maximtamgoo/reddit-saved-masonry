59a3ab2325bb6428b2f05ef90c30641a97be9e81
Maximtamgoo <maximtamgoo@gmail.com>
2026-04-03 12:46:13 -0400

    log just the response

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -2,13 +2,11 @@ import { createMiddleware } from "hono/factory";
 
 export const logger = createMiddleware(async (c, next) => {
   const start = performance.now();
-  const isoStart = new Date().toISOString();
+  const isoDate = new Date().toISOString();
+  await next();
   const method = c.req.method;
   const path = c.req.path;
-  console.log(`<-- [${isoStart}] ${method} ${path}`);
-  await next();
   const status = c.res.status;
-  const isoEnd = new Date().toISOString();
   const duration = performance.now() - start;
-  console.log(`--> [${isoEnd}] ${method} ${path} ${status} ${duration.toFixed()}ms`);
+  console.log(`[${isoDate}] ${method} ${path} ${status} ${duration.toFixed()}ms`);
 });
